6e7d1cd7a8a21e633f434ff80921ad931bffc98a
KINGSLEY <mamahkingsleychukwuebuka@gmail.com>
2023-09-08 17:33:49 +0100

    main

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function updatedate(){
     const daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const now = new Date();
     const dayOfWeek = daysOfWeek[now.getUTCDay()];
-    document.querySelector(".CurrDayodtheWeek").textContent = dayOfWeek;
+    document.querySelector(".CurrentDayodtheWeek").textContent = dayOfWeek;
 }
 updatedate();
 var time = new Date().getTime();
-document.querySelector(".time").innerHTML = time;
+document.querySelector(".currenttime").innerHTML = time;
